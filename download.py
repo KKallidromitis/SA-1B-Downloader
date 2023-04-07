ba50838d736d00a59bf5b7e29e2d4c94e1b20f23
KKallidromitis <kk984@cornell.edu>
2023-04-07 16:23:21 -0700

    split images and masks

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ import argparse
 import requests
 
 def download_and_extract(args):
-    file_name, url, raw_dir, data_dir = args
+    file_name, url, raw_dir, images_dir, masks_dir = args
     
     # Check if the file already exists
     if not os.path.exists(f'{raw_dir}/{file_name}'):
@@ -21,7 +21,11 @@ def download_and_extract(args):
     # Extract the file
     print(f'Extracting {file_name}...')
     with tarfile.open(f'{raw_dir}/{file_name}') as tar:
-        tar.extractall(path=data_dir)
+        for member in tar.getmembers():
+            if member.name.endswith(".jpg"):
+                tar.extract(member, path=images_dir)
+            elif member.name.endswith(".json"):
+                tar.extract(member, path=masks_dir)
         
     print(f'{file_name} extracted!')
 
@@ -30,7 +34,8 @@ parser = argparse.ArgumentParser(description='Download and extract files.')
 parser.add_argument('--processes', type=int, default=4, help='Number of processes to use for downloading and extracting files.')
 parser.add_argument('--input_file', type=str, default='sa1b_links.txt', help='Path to the input file containing file names and URLs.')
 parser.add_argument('--raw_dir', type=str, default='raw', help='Directory to store downloaded files.')
-parser.add_argument('--data_dir', type=str, default='images', help='Directory to store extracted files.')
+parser.add_argument('--images_dir', type=str, default='images', help='Directory to store extracted image files.')
+parser.add_argument('--masks_dir', type=str, default='annotations', help='Directory to store extracted JSON mask files.')
 args = parser.parse_args()
 
 # Read the file names and URLs
@@ -39,10 +44,11 @@ with open(args.input_file, 'r') as f:
 
 # Create the directories if they do not exist
 os.makedirs(args.raw_dir, exist_ok=True)
-os.makedirs(args.data_dir, exist_ok=True)
+os.makedirs(args.images_dir, exist_ok=True)
+os.makedirs(args.masks_dir, exist_ok=True)
 
 # Download and extract the files in parallel
 with Pool(processes=args.processes) as pool:
-    pool.map(download_and_extract, [line.strip().split('\t') + [args.raw_dir, args.data_dir] for line in lines])
+    pool.map(download_and_extract, [line.strip().split('\t') + [args.raw_dir, args.images_dir, args.masks_dir] for line in lines])
 
 print('All files extracted successfully!')
